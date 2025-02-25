38ca4852aba9323fe288abf2f5bb60993bdfb056
Jose Ignacio Tamayo <jose.ignacio.tamayo@gmail.com>
2025-02-25 06:27:22 +0100

    Add Variable descriptions

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,15 +1,16 @@
 variable "aws_account_id" {
   type        = string
-  description = "AWS Account ID"
+  description = "AWS Account ID to deploy the resources to"
 }
 
 variable "aws_region" {
   type        = string
-  description = "AWS Region to use"
+  description = "AWS Region to deploy the resources to"
 }
 
 variable "aws_cli_profile" {
-  type = string
+  type        = string
+  description = "AWS Cli profile to use to interact with AWS"
 }
 
 variable "vpc" {
@@ -17,6 +18,7 @@ variable "vpc" {
     cidr = string
     name = string
   })
+  description = "VPC to deploy the resources in"
 }
 
 variable "tags" {
@@ -25,11 +27,13 @@ variable "tags" {
 }
 
 variable "ssh_publickey_file" {
-  type = string
+  type        = string
+  description = "Public key to upload to AWS as a KeyPair. Use the private key to SSH to VMs"
 }
 
 variable "vm_iam_role" {
-  type = string
+  type        = string
+  description = "IAM Role to attach to VMs"
 }
 
 variable "entropy" {
@@ -38,33 +42,36 @@ variable "entropy" {
   default     = "DoNotChange"
 }
 
-# Variables named just like the S3 Backend config
 variable "use_mirror" {
-  type    = bool
-  default = false
+  type        = bool
+  default     = false
+  description = "True to read as data the same TerraformState and expose additional outputs"
 }
 
 
+# Variables named just like the S3 Backend config
 variable "bucket" {
-  type      = string
-  sensitive = true
-  default   = ""
+  type        = string
+  sensitive   = true
+  description = "S3 Bucket name"
 }
 
 variable "region" {
-  type      = string
-  sensitive = true
-  default   = ""
+  type        = string
+  sensitive   = true
+  description = "S3 Bucket Region"
 }
 
 variable "key" {
-  type      = string
-  sensitive = true
-  default   = ""
+  type        = string
+  sensitive   = true
+  default     = ""
+  description = "Path in the S3 bucket to the TerraformState"
 }
 
 variable "profile" {
-  type      = string
-  sensitive = true
-  default   = ""
+  type        = string
+  sensitive   = true
+  default     = ""
+  description = "AWS Cli Profile used to operate remote TerraformState in the S3 Bucket"
 }
